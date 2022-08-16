93469aa63508ad6a487f65e6959bae99b8925724
Richard Blanchette <richardphi1618@gmail.com>
2022-08-15 22:16:13 -0400

    feat(main): able to add a category based on what is in categories.yml

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 from collections import defaultdict
 from os import listdir, path, remove, scandir
 from typing import List
 
 import pandas as pd
+import yaml
 
 # df = pd.read_csv (r'./data/shared_savings.CSV')
 # print (df)
@@ -53,11 +55,41 @@ def read_all_csv_to_df (dir: str):
 def transaction_classifier ( df ):
     return df
 
+def to_upper(oldList):
+    newList = []
+    for element in oldList:
+        newList.append(element.upper())
+    return newList
+
+def find_category(input_dict, target):
+    solutions = []
+    for key, value in input_dict.items():
+        for i in to_upper(value):
+            if i in target.upper():
+                solutions.append(key)
+    
+    if solutions == []: solutions = ['None']
+        
+    return solutions[0]
+
+def read_yml_as_dict(file_path: str):
+    if path.isfile(file_path):
+        f = open(file_path, "r")
+        input = f.read()
+        f.close()
+    
+    try:
+        output = yaml.safe_load(input)
+    except Exception as e:
+        print(str(e))
+        sys.exit(1) 
+    
+    return output
+
 if __name__=='__main__':
 
     master_raw_file_path = f'./{data_dir}/master_raw.csv'
     master_processed_file_path = f'./{data_dir}/master_processed.csv'
-
 
     if path.exists(master_raw_file_path): remove(master_raw_file_path)
     if path.exists(master_processed_file_path): remove(master_processed_file_path)
@@ -79,5 +111,11 @@ if __name__=='__main__':
 
     output = transaction_classifier(output)
 
+    
+
+    categories = read_yml_as_dict('./categories.yml')
+
+    output['Categories'] = output.apply(lambda x: find_category(categories, x['Description']), axis=1)
+    
     print(output.info())
     output.to_csv(master_processed_file_path)
